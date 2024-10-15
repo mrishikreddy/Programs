2433acf65cc35a1b2df46076f57a648bb9a16feb
mrishikreddy <malerishikreddy@gmail.com>
2024-10-15 19:37:17 +0530

    practiced c programs

---
+++ c/132_radix_sort.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+#include<math.h>
+int size;
+void countSort(int arr[size],int expp)
+{
+    int out[100] = {0}, count[10] = {0},i,index;
+    for(i=0;i<size;i++)
+    {
+        index = arr[i]/expp;
+        count[index%10]+=1;
+    }
+    
+    for(i=1;i<10;i++)
+        count[i]+=count[i-1];
+    i = size-1;
+    while(i>=0)
+    {
+        index = arr[i]/expp;
+        out[count[index%10]-1]=arr[i];
+        count[index%10]-=1;
+        i-=1;
+    }
+    for(i=0;i<size;i++)
+        arr[i] = out[i];  
+    
+}
+
+void radixSort(int arr[size])
+{
+   int max=0,i;
+   for(i=0;i<size;i++)
+   {
+       if(max<arr[i])
+       max = arr[i];
+   }
+   int expp = 1;
+   while(max/expp>=1)
+   {
+       countSort(arr,expp);
+       expp *= 10;
+   }
+}
+
+int main()
+{
+    int arr[20],i;
+    printf("enter the size of array:");
+    scanf("%d",&size);
+    printf("enter %d elements: ",size);
+    for(i=0;i<size;i++)
+        scanf("%d",&arr[i]);
+    radixSort(arr);
+    for(i=0;i<size;i++)
+        printf("%d ",arr[i]);   
+}
--- c/777_practice.c
+++ c/777_practice.c
@@ -1,31 +1,8 @@
-// insertion sort
-
 #include<stdio.h>
+
+
+
 int main()
 {
-    int arr[10],i,j,temp,size,key,ind;
-    printf("enter the size of array: ");
-    scanf("%d",&size);
-    printf("enter %d elements: ",size);
-    for(i=0;i<size;i++)
-        scanf("%d",&arr[i]);
-
-    for(i=1;i<size;i++)
-    {
-        key = arr[i];
-        j = i-1;
-        while(j>-1 && arr[j]>key)
-        {
-            arr[j+1] = arr[j];
-            j--;
-        }
-        arr[j+1] = key;
-
-    }
-
-    printf("The sorted array is: ");
-    for(i=0;i<size;i++)
-        printf("%d ",arr[i]);
-
-    return 0;
+    
 }
