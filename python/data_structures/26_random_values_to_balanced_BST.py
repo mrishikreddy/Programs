2959e1c00b49984b7a4b1ba00dd8b2033537aff4
mrishikreddy <malerishikreddy@gmail.com>
2024-11-06 11:38:01 +0530

    edited java program

---
+++ python/data_structures/26_random_values_to_balanced_BST.py
@@ -0,0 +1,17 @@
+class Node:
+    def __init__(self,val):
+        self.left = None
+        self.right = None
+        self.data = val
+
+def insertValues(val):
+    if root is None:
+        return Node(val)
+    
+
+
+l = list(map(int,input("enter the values of elements: ").split(' ')))
+l.sort()
+
+for i in l:
+    
